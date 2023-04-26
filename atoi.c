929ced272b79e26c86fd2f826300bb7b0d29a1e6
Mub2023 <mubatta@gmail.com>
2023-04-25 20:08:52 -0700

    ^_^ new team project @_@

--- atoi.c
+++ atoi.c
@@ -1,18 +1,21 @@
 #include "shell.h"
+
 /**
- * interactive - return ture if shell is interactive mode.
+ * interactive - returns true if shell is interactive mode
  * @info: struct address
- * Return: 1 if interactive or 0 otherwise.
+ *
+ * Return: 1 if interactive mode, 0 otherwise
  */
 int interactive(info_t *info)
 {
 	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
+
 /**
- * is_delim - checks if character is delimeter.
- * @c: the char to check.
- * @delim: the delimeter string.
- * Return: 1 if ture , 0 otherwise.
+ * is_delim - checks if character is a delimeter
+ * @c: the char to check
+ * @delim: the delimeter string
+ * Return: 1 if true, 0 if false
  */
 int is_delim(char c, char *delim)
 {
@@ -21,11 +24,13 @@ int is_delim(char c, char *delim)
 			return (1);
 	return (0);
 }
+
 /**
- * _isalpha - checks for alphabetic.
- * @c: the char to input.
+ * _isalpha - checks for alphabetic character
+ * @c: The character to input
  * Return: 1 if c is alphabetic, 0 otherwise
  */
+
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
@@ -33,31 +38,34 @@ int _isalpha(int c)
 	else
 		return (0);
 }
+
 /**
- * _atoi - converts a string to int.
+ * _atoi - converts a string to an integer
  * @s: the string to be converted
  * Return: 0 if no numbers in string, converted number otherwise
  */
+
 int _atoi(char *s)
 {
-	int x, si = 1, f = 0, output;
+	int i, sign = 1, flag = 0, output;
 	unsigned int result = 0;
 
-	for (x = 0; s[x] != '\0' && f != 2; x++)
+	for (i = 0; s[i] != '\0' && flag != 2; i++)
 	{
-		if (s[x] == '-')
-			si *= -1;
+		if (s[i] == '-')
+			sign *= -1;
 
-		if (s[x] >= '0' && s[x] <= '9')
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-		f = 1;
-		result *= 10;
-		result += (s[x] - '0');
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
 		}
-		else if (f == 1)
-			f = 2;
+		else if (flag == 1)
+			flag = 2;
 	}
-	if (si == -1)
+
+	if (sign == -1)
 		output = -result;
 	else
 		output = result;
