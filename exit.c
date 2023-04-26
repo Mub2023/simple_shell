d088c57e03002cd293d58b9591c6032aadaacf29
Mub2023 <mubatta@gmail.com>
2023-04-26 04:08:37 -0700

    ^_^ new team project @_@

--- exit.c
+++ exit.c
@@ -1,61 +1,67 @@
 #include "shell.h"
+
 /**
- * *_strncpy - copies a string
- * @dest: the destination string to copied to.
- * @src: the source string.
- * @n: te amount of char to be copied
- * Return: the concatened string.
+ **_strncpy - copies a string
+ *@dest: the destination string to be copied to
+ *@src: the source string
+ *@n: the amount of characters to be copied
+ *Return: the concatenated string
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int x = 0, y;
+	int i, j;
 	char *s = dest;
 
-	while (src[x] != '\0' && x < n - 1)
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
 	{
-		dest[x] = src[x];
-		x++;
+		dest[i] = src[i];
+		i++;
 	}
-	if (x < n)
+	if (i < n)
 	{
-		y = x;
-		while (y < n)
+		j = i;
+		while (j < n)
 		{
-			dest[y] = '\0';
-			y++;
+			dest[j] = '\0';
+			j++;
 		}
 	}
 	return (s);
 }
+
 /**
- * *_strncat - concatenates two string.
- * @dest: the first srting.
- * @src: the second string.
- * @n: the amount of bytes to used
- * Return: the concatenated string
+ **_strncat - concatenates two strings
+ *@dest: the first string
+ *@src: the second string
+ *@n: the amount of bytes to be maximally used
+ *Return: the concatenated string
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int x = 0, y = 0;
+	int i, j;
 	char *s = dest;
 
-	while (dest[x] != '\0')
-		x++;
-	while (src[y] != '\0' && y < n)
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
 	{
-		dest[x] = src[y];
-		x++;
-		y++;
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
-	if (y < n)
-		dest[x] = '\0';
+	if (j < n)
+		dest[i] = '\0';
 	return (s);
 }
+
 /**
- * *_strchr - locates a character in a string
- * @s: the string to be parsed.
- * @c: the char to look for.
- * Return: (s)
+ **_strchr - locates a character in a string
+ *@s: the string to be parsed
+ *@c: the character to look for
+ *Return: (s) a pointer to the memory area s
  */
 char *_strchr(char *s, char c)
 {
